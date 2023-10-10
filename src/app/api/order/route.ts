40a895ca24871d33752d75b830074e78c5d85905
shalom99 <castroshalom77@gmail.com>
2023-10-11 07:57:48 +0800

    nodemailer embed

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -13,6 +13,9 @@ export async function POST(request: NextRequest) {
       return base64String;
     }
   }
+
+
+
   
 
   const {
@@ -29,6 +32,64 @@ export async function POST(request: NextRequest) {
     orderNotes,
     cartItems
   } = await request.json();
+
+  const img2 = `
+  <html>
+    <head>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+        }
+        h1 {
+          color: #007BFF;
+        }
+        p {
+          font-size: 16px;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Test Order #</h1>
+      <ul>
+        <li>First Name: ${firstName}</li>
+        <li>Last Name:  ${lastName}</li>
+        <li>Company Name:  ${companyName ? companyName : 'N/A'}</li>
+        <li>Street Address 1:  ${streetAddress1}</li>
+        <li>Suburb:  ${suburb}</li>
+        <li>State:  ${state}</li>
+        <li>Postcode:  ${postcode}</li>
+        <li>Phone:  ${phone}</li>
+        <li>Email:  ${email}</li>
+        <li>Order Notes: ${orderNotes}</li>
+        <li>Order Items: ${cartItems[0].name}</li>
+      </ul>
+      <div>
+    
+      <img width="100" height="100" src="cid:unique2@nodemailer.com" alt="Cap Image Front" />
+     
+
+      </div>
+
+    </body>
+  </html>
+`
+
+
+let imgAttachments = [];
+
+for (const obj of cartItems) {
+  const nestedImages = obj.views;
+  for (const key in nestedImages) {
+    imgAttachments.push({
+      filename: `${obj.name}_${key}.png`,
+      encoding: 'base64',
+      content: removeBase64Prefix(nestedImages[key]),
+      cid: `${obj.name.toLowerCase()}_${key}@nodemailer.com`
+    });
+  }
+}
+
+
 
   const transport = nodemailer.createTransport({
     service: 'gmail',
@@ -52,64 +113,40 @@ export async function POST(request: NextRequest) {
     to: process.env.TO_EMAIL,
     // cc: email, (uncomment this line if you want to send a copy to the sender)
     subject: `Order Email from ${firstName + " " + lastName}  (${email})`,
-    text: `
-    First Name: ${firstName}
-    Last Name:  ${lastName}
-    Company Name:  ${companyName ? companyName : 'N/A'}
-    Street Address 1:  ${streetAddress1}
-    Street Address 2:  ${streetAddress2}
-    State:  ${state}
-    Postcode:  ${postcode}
-    phone:  ${phone}
-    Email:  ${email}
-    Order Notes: ${orderNotes}
-    Order Items: ${cartItems[0].name}
-    `,
     html: `
     <html>
-      <head>
-        <style>
-          body {
-            font-family: Arial, sans-serif;
-          }
-          h1 {
-            color: #007BFF;
-          }
-          p {
-            font-size: 16px;
-          }
-        </style>
-      </head>
       <body>
-        <h1>Test Order #</h1>
-        <ul>
-          <li>First Name: ${firstName}</li>
-          <li>Last Name:  ${lastName}</li>
-          <li>Company Name:  ${companyName ? companyName : 'N/A'}</li>
-          <li>Street Address 1:  ${streetAddress1}</li>
-          <li>State:  ${state}</li>
-          <li>Postcode:  ${postcode}</li>
-          <li>Phone:  ${phone}</li>
-          <li>Email:  ${email}</li>
-          <li>Order Notes: ${orderNotes}</li>
-          <li>Order Items: ${cartItems[0].name}</li>
-        </ul>
-        <div>
-      
-        <img src=${cartItems[0].views.front} alt="Cap Image Front" width="100" height="100" />
-
-        </div>
-
+        <h1>Embedded Image Example</h1>
+        ${imgAttachments.map(attachment => `
+        <img src="cid:${attachment.cid}" alt="${attachment.filename}" width="100">
+      `).join('')}
       </body>
     </html>
   `,
-  attachments: [
-    {
-      filename: 'image.png',  // Name of the attached file
-      encoding: 'base64',
-      content: removeBase64Prefix(cartItems[0].views.front)
-    }
-  ]
+
+  attachments: imgAttachments
+    // {
+    //   filename: 'image.png',  // Name of the attached file
+    //   encoding: 'base64',
+    //   content: removeBase64Prefix(cartItems[0].views.front)
+    // },
+    // {
+    //   filename: 'cat.jpg',
+    //   content: cartItems[0].views.front.split("base64,")[1],
+    //   encoding: 'base64'
+    // },
+    // {
+    //   filename: 'image2.png',
+    //   content: removeBase64Prefix(cartItems[0].views.front),
+    //   cid: 'unique@nodemailer.com'
+    // },
+    // {
+    //   filename: 'image3.png',  // Name of the attached file
+    //   encoding: 'base64',
+    //   content: removeBase64Prefix(cartItems[0].views.front),
+    //   cid: 'unique2@nodemailer.com' // Unique identifier for the embedded image
+    // }
+  
   };
 
   const sendMailPromise = () =>
